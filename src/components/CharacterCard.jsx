16f443e832ca4817267358fa9d0abb620af5877d
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-01-26 17:08:03 +0100

    Bug regler sur les import pour le build

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import "../styles/components/CharacterCard.css";
+import "../styles/components/characterCard.css";
 import notFoundImage from "../assets/not-found.png";
 
 const CharacterCard = ({ character }) => {

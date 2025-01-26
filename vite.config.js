71b7cddda057e6033f7e5f3863544629dbdb5825
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-01-26 17:17:48 +0100

    test pour le lancement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://honkai-star-rail-backend.onrender.com",
+        changeOrigin: true,
+        secure: false, // à utiliser uniquement en dev
+      },
+    },
+  },
 });
